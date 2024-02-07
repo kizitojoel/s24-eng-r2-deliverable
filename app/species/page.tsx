a299b5060fafc3d57cd55d313d49178efd0bd59f
Joel Kizito <kizitojoel16@gmail.com>
2024-02-06 20:37:58 -0500

    Complete working version

--- app/species/page.tsx
+++ app/species/page.tsx
@@ -30,6 +30,7 @@ export default async function SpeciesList() {
       </div>
       <Separator className="my-4" />
       <div className="flex flex-wrap justify-center">
+        {/* Pass in the sessionId to this SpeciesCard so that we can tell who authored it */}
         {species?.map((species) => <SpeciesCard key={species.id} species={species} />)}
       </div>
     </>
